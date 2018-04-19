f91f00cc514dc60db63c605d937f3451eced0d91
Susan Lin <slin15@stuy.edu>
2018-04-18 21:07:08 -0400

    ordering

--- Deque.java
+++ Deque.java
@@ -1,14 +1,5 @@
 public interface Deque<Card> {
 
-// /Returns the number of elements in this deque.
-    public int size(); 
-
-//Inserts the specified element into the queue represented by this deque 
-//(in other words, at the tail of this deque) if it is possible to do so 
-//immediately without violating capacity restrictions, returning true upon success 
-//and throwing an IllegalStateException if no space is currently available.
-    public boolean add(Card e); 
-
 //Inserts the specified element at the front of this deque if it is possible to 
 //do so immediately without violating capacity restrictions.
     public void	addFirst(Card e); 
@@ -17,15 +8,14 @@ public interface Deque<Card> {
 //do so immediately without violating capacity restrictions.
     public void	addLast(Card e); 
 
-//Returns true if this deque contains the specified element.
-    public boolean contains(Object o); 
+// /Returns the number of elements in this deque.
+    public int size(); 
 
-//Returns an iterator over the elements in this deque in reverse sequential order.
-    public Iterator<Card> descendingIterator(); 
+//Retrieves and removes the first element of this deque.
+    public Card	removeFirst(); 
 
-//Retrieves, but does not remove, the head of the queue represented by this deque 
-//(in other words, the first element of this deque).
-    public Card element(); 
+//Retrieves and removes the last element of this deque.
+    public Card	removeLast(); 
 
 //Retrieves, but does not remove, the first element of this deque.
     public Card getFirst(); 
@@ -33,8 +23,29 @@ public interface Deque<Card> {
 //Retrieves, but does not remove, the last element of this deque.
     public Card getLast(); 
 
-//Returns an iterator over the elements in this deque in proper sequence.
-    public Iterator<Card> iterator(); 
+//Inserts the specified element at the front of this deque unless it would violate capacity restrictions.
+    public boolean offerFirst(Card e); 
+
+//Retrieves and removes the first element of this deque, or returns null if this deque is empty.
+    public Card	pollFirst(); 
+
+//Retrieves, but does not remove, the first element of this deque, or returns null if this deque is empty.
+    public Card peekFirst(); 
+
+//Inserts the specified element at the end of this deque unless it would violate capacity restrictions. 
+    public boolean offerLast(Card e); 
+
+//Retrieves and removes the last element of this deque, or returns null if this deque is empty.
+    public Card	pollLast(); 
+
+//Retrieves, but does not remove, the last element of this deque, or returns null if this deque is empty.
+    public Card	peekLast();
+
+//Inserts the specified element into the queue represented by this deque 
+//(in other words, at the tail of this deque) if it is possible to do so 
+//immediately without violating capacity restrictions, returning true upon success 
+//and throwing an IllegalStateException if no space is currently available.
+    public boolean add(Card e); 
 
 //Inserts the specified element into the queue represented by this deque 
 //(in other words, at the tail of this deque) if it is possible to do so immediately 
@@ -42,31 +53,22 @@ public interface Deque<Card> {
 //no space is currently available.
     public boolean offer(Card e); 
 
-//Inserts the specified element at the front of this deque unless it would violate capacity restrictions.
-    public boolean offerFirst(Card e); 
-
-//Inserts the specified element at the end of this deque unless it would violate capacity restrictions. 
-    public boolean offerLast(Card e); 
-
-//Retrieves, but does not remove, the head of the queue represented by this deque 
-//(in other words, the first element of this deque), or returns null if this deque is empty.
-    public Card	peek(); 
-
-//Retrieves, but does not remove, the first element of this deque, or returns null if this deque is empty.
-    public Card peekFirst(); 
-
-//Retrieves, but does not remove, the last element of this deque, or returns null if this deque is empty.
-    public Card	peekLast(); 
+ //Retrieves and removes the head of the queue represented by this deque 
+//(in other words, the first element of this deque).
+    public Card	remove(); 
 
 //Retrieves and removes the head of the queue represented by this deque 
 //(in other words, the first element of this deque), or returns null if this deque is empty.
     public Card	poll(); 
 
-//Retrieves and removes the first element of this deque, or returns null if this deque is empty.
-    public Card	pollFirst(); 
+//Retrieves, but does not remove, the head of the queue represented by this deque 
+//(in other words, the first element of this deque).
+    public Card element(); 
 
-//Retrieves and removes the last element of this deque, or returns null if this deque is empty.
-    public Card	pollLast(); 
+//Retrieves, but does not remove, the head of the queue represented by this deque 
+//(in other words, the first element of this deque), or returns null if this deque is empty.
+    public Card	peek();  
+
 //Pops an element from the stack represented by this deque.
     public Card	pop(); 
 
@@ -76,21 +78,20 @@ public interface Deque<Card> {
 //f no space is currently available.
     public void	push(Card e); 
 
-//Retrieves and removes the head of the queue represented by this deque 
-//(in other words, the first element of this deque).
-    public Card	remove(); 
+//Returns true if this deque contains the specified element.
+    public boolean contains(Object o); 
+
+//Returns an iterator over the elements in this deque in reverse sequential order.
+    public Iterator<Card> descendingIterator(); 
+
+//Returns an iterator over the elements in this deque in proper sequence.
+    public Iterator<Card> iterator(); 
 
 //Removes the first occurrence of the specified element from this deque.
     public boolean remove(Object o); 
 
-//Retrieves and removes the first element of this deque.
-    public Card	removeFirst(); 
-
 //Removes the first occurrence of the specified element from this deque.
     public boolean removeFirstOccurrence(Object o); 
-
-//Retrieves and removes the last element of this deque.
-    public Card	removeLast(); 
 
 //Removes the last occurrence of the specified element from this deque.
     public boolean	removeLastOccurrence(Object o); 
